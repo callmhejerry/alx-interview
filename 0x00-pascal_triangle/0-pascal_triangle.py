3e6040e51408d46ee337cde38eafb3c170f9f678
chinedu jeremiah <chinedujeremiah2002@gmail.com>
2023-06-29 11:26:54 +0100

    added a new project

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+
+'''
+A function that returns a list of lists
+of integers representing the Pascal’s triangle 
+of n
+'''
+
+def pascal_triangle(n):
+    '''
+    return the pascal triangle for n
+    '''
+    if (n <= 0):
+        return [];
+    
+    return calc(n)
+
+def calc(n, results=[]):
+    '''
+    calculate the pascal triangle
+    '''
+    if (n == 1):
+        results.append([1])
+        return results
+    triangle = calc(n - 1, results)
+    new = []
+    last = triangle[len(triangle) - 1]
+    prev = 0
+    
+    for i in last:
+        new.append(i + prev)
+        prev = i
+    new.append(prev)
+    results.append(new)
+    return results
