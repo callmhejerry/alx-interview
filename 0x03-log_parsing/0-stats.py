3b3f44f03c71cedc8bb5022b1a0579df831293aa
chinedu jeremiah <chinedujeremiah2002@gmail.com>
2023-12-23 10:55:48 +0100

    done task 0

---
+++ 0x03-log_parsing/0-stats.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+
+import sys
+import re
+
+count = 1
+status_codes = {
+    "200": 0,
+    "301": 0,
+    "400": 0,
+    "401": 0,
+    "403": 0,
+    "404": 0,
+    "405": 0,
+    "500": 0
+}
+file_size = 0
+
+try:
+    for user_input in sys.stdin:
+        if (count == 10):
+            print("File Size: {}".format(file_size))
+            for key, val in status_codes.items():
+                if (val > 0):
+                    print("{}: {}".format(key, val))
+            count = 1
+
+        pattern = r'\b(\d{3})\s(\d+)\b'
+        matches = re.search(pattern, user_input)
+
+        if matches:
+            status_code = matches.group(1)
+
+            status_codes[status_code] = status_codes[status_code] + 1
+            file_size = file_size + int(matches.group(2))
+
+        count = count + 1
+except KeyboardInterrupt:
+    print("File Size: {}".format(file_size))
+    for key, val in status_codes.items():
+        if (val > 0):
+            print("{}: {}".format(key, val))
+    count = 1
