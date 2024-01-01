c906e51e86621959f29fe69b4508acbb0da9e5ee
chinedu jeremiah <chinedujeremiah2002@gmail.com>
2024-01-01 17:43:32 +0100

    Changed the algorithm of the function

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -12,31 +12,14 @@ If n is impossible to achieve, return 0
 """
 
 
-def get_factors(n):
-    """Return a list of tuples for the factors of n"""
-    factors = []
-
-    for i in range(1, n + 1):
-        if n // i <= 1:
-            break
-        else:
-            if n % i == 0:
-                factors.append({
-                    "factors": (i, n // i),
-                    "sum_factors": i + (n // i)
-                })
-    return factors
-
-
 def minOperations(n):
     """Returns the minimum operations needed to result in exactly n H"""
-    if n <= 1 or n > 50:
+    if n <= 1:
         return 0
+    result = 0
+    for i in range(2, n + 1):
+        while n % i == 0:
+            result += i
+            n = n // i
 
-    factors = get_factors(n)
-    lowest_factor = factors[0]["sum_factors"]
-
-    for factor in factors:
-        if factor["sum_factors"] <= lowest_factor:
-            lowest_factor = factor["sum_factors"]
-    return lowest_factor
+    return result
