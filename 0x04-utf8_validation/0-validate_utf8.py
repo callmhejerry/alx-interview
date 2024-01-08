5f34f3245d6065dad211486610ce8a3c6ace4c20
chinedu jeremiah <chinedujeremiah2002@gmail.com>
2024-01-08 23:36:47 +0100

    Added the function to validate a utf encoding data

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -10,7 +10,28 @@ def validUTF8(data):
     valid UTF-8 encoding."""
     for char in data:
         binary_rep = bin(char)[2:]
-        if len(binary_rep) >= 8:
+        if check_binary(binary_rep) is False:
             return False
+    return True
+
+
+def check_binary(bin):
+    """check the binary representation"""
+    length_of_str = len(bin)
+
+    if length_of_str >= 0 and length_of_str <= 8:
+        if length_of_str > 7:
+            return False
+    if length_of_str >= 9 and length_of_str <= 16:
+        if length_of_str > 11:
+            return False
+    if length_of_str >= 17 and length_of_str <= 24:
+        if length_of_str > 16:
+            return False
+    if length_of_str >= 25 and length_of_str <= 32:
+        if length_of_str > 21:
+            return False
+    if length_of_str > 32:
+        return False
 
     return True
