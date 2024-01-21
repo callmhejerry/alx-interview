486890498db164bd42611065beab2911b684fdc1
chinedu jeremiah <chinedujeremiah2002@gmail.com>
2024-01-21 01:09:22 +0100

    style fix

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -4,26 +4,26 @@ const request = require('request');
 const movieId = process.argv[2];
 const movieEndPoint = 'https://swapi-api.alx-tools.com/api/films/' + movieId;
 
-function makeRequest(characterList, index) {
-    if (characterList.length === index) {
-        return;
-    }
+function makeRequest (characterList, index) {
+  if (characterList.length === index) {
+    return;
+  }
 
-    request(characterList[index], (error, response, body) => {
-        if (error) {
-            console.log(error);
-        } else {
-            console.log(JSON.parse(body).name);
-            makeRequest(characterList, index + 1);
-        }
-    });
+  request(characterList[index], (error, response, body) => {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log(JSON.parse(body).name);
+      makeRequest(characterList, index + 1);
+    }
+  });
 }
 
 request(movieEndPoint, (error, response, body) => {
-    if (error) {
-        console.log(error);
-    } else {
-        const characterList = JSON.parse(body).characters;
-        makeRequest(characterList, 0);
-    }
+  if (error) {
+    console.log(error);
+  } else {
+    const characterList = JSON.parse(body).characters;
+    makeRequest(characterList, 0);
+  }
 });
